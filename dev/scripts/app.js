52fb09bf00e6ab9b60b2cb41e0af5e4751728fdb
Corey Lanier <iam@coreylanier.com>
2018-07-21 17:03:43 -0400

    first commit

---
+++ dev/scripts/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello
+        </div>
+      )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
