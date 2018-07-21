dfb392e754d540d011fb49b124597d24bfafa039
Corey Lanier <iam@coreylanier.com>
2018-07-21 17:47:52 -0400

    adding flag css, favicons, images

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyD_L93QGk5L-B8tkewVe8kUCrsK_DjIfFQ",
+  authDomain: "sailormoon-elo.firebaseapp.com",
+  databaseURL: "https://sailormoon-elo.firebaseio.com",
+  projectId: "sailormoon-elo",
+  storageBucket: "",
+  messagingSenderId: "841627538839"
+};
+firebase.initializeApp(config);
 
 class App extends React.Component {
     render() {
