c5e13832bd310edcab1bd8576bb7d3b4a6e1e796
Corey Lanier <iam@coreylanier.com>
2018-07-21 20:53:37 -0400

    styling table

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -18,19 +18,81 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      loggedIn: false,
+      playerOne: '',
+      playerOneELO: '',
+      playerOneNewELO: '',
+      playerTwo: '',
+      playerTwoELO: '',
+      playerTwoNewELO: '',
+      winner: '',
+      rankings: [],
+      playerCountry: '',
+      playerMain: '',
+      adminEmail: '',
+      adminPwd: ''
     };
   }
 
   render() {
-    return (
-      <div>
+    return <div>
         <div className="jumbotron text-center">
           <h1 className="page-header">Sailor Moon S Global Rankings</h1>
           <p>Who's best at fighting evil by moonlight?</p>
         </div>
-      </div>
-    )
+        <div className="container">
+          <table className="table table-striped table-bordered">
+            <thead className="thead-dark text-center">
+              <tr>
+                <th>Rank</th>
+                <th>Player</th>
+                <th>Main</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tr>
+              <th>1</th>
+              <td>
+                <img src="flags/blank.gif" className="flag flag-ca" alt="Canada" /> TTT NeoRussell
+              </td>
+              <td className="main-char">
+                <img src="img/pluto.png" alt="Sailor Pluto" />
+              </td>
+              <td>1600</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>
+                <img src="flags/blank.gif" className="flag flag-ca" alt="Canada" /> SMRT SRK Missing Person
+              </td>
+              <td className="main-char">
+                <img src="img/uranus.png" alt="Sailor Uranus" /> <img src="img/mercury.png" alt="Sailor Mercury" />
+              </td>
+              <td>1500</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>
+                <img src="flags/blank.gif" className="flag flag-us" alt="United States" /> Echo Fox Justin Wong
+              </td>
+              <td className="main-char">
+                <img src="img/mars.png" alt="Sailor Mars" />
+              </td>
+              <td>1490</td>
+            </tr>
+            <tr>
+              <th>4</th>
+              <td>
+                <img src="flags/blank.gif" className="flag flag-ca" alt="Canada" /> 7PA Quiet Anger
+              </td>
+              <td className="main-char">
+                <img src="img/mercury.png" alt="Sailor Mercury" /> <img src="img/neptune.png" alt="Sailor Neptune" />
+              </td>
+              <td>1480</td>
+            </tr>
+          </table>
+        </div>
+      </div>;
   }
 }
 
