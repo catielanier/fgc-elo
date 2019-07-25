1f18ddbc1f8cb72aab293973f6eba1412529c972
Corey <iam@coreylanier.com>
2019-07-25 18:58:59 -0400

    created key components

---
+++ v2/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Sailor Moon S Global Rankings</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
