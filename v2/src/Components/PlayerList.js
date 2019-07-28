ed3efca70020148579e1e0010f3ddffcfb6525f9
Corey <iam@coreylanier.com>
2019-07-28 13:45:28 -0400

    created sorting function between tournament score/elo

--- v2/src/Components/PlayerList.js
+++ v2/src/Components/PlayerList.js
@@ -5,14 +5,17 @@ import firebase from "../firebase";
 class PlayerList extends React.Component {
   state = {
     playerList: [],
-    loading: false
+    loading: false,
+    sorting: "tournamentScore"
   };
   async componentDidMount() {
+    this.setState({
+      loading: true
+    });
     this.dbRefPlayers = firebase.database().ref("players/");
     let playerList = [];
     await this.dbRefPlayers.on("value", snapshot => {
       const data = snapshot.val();
-      console.log(data);
       for (let key in data) {
         playerList.push({
           key,
@@ -25,13 +28,53 @@ class PlayerList extends React.Component {
         );
       });
       this.setState({
-        playerList
+        playerList,
+        loading: false
       });
     });
   }
+
+  changeSort = e => {
+    let { playerList } = this.state;
+    const sorting = e.target.value;
+    if (sorting === "tournamentScore") {
+      playerList.sort(function(x, y) {
+        return (
+          y.tournamentScore - x.tournamentScore ||
+          y.elo - x.elo ||
+          x.name.localeCompare(y.name)
+        );
+      });
+    } else {
+      playerList.sort(function(x, y) {
+        return (
+          y.elo - x.elo ||
+          y.tournamentScore - x.tournamentScore ||
+          x.name.localeCompare(y.name)
+        );
+      });
+    }
+    this.setState({
+      playerList,
+      sorting
+    });
+  };
+
   render() {
     return (
       <section className="player-list">
+        <label htmlFor="sorting">
+          Sort by:
+          <select
+            name="sorting"
+            onChange={this.changeSort}
+            defaultValue={this.state.sorting}
+            value={this.state.sorting}
+          >
+            <option value="tournamentScore">Tournament Score</option>
+            <option value="elo">Ladder Rating</option>
+          </select>
+        </label>
         <div className="grid-container">
           <div className="grid-row grid-header">
             <div>Rank</div>
