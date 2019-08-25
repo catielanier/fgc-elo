f878982832ba37d569c501167dca965df4783461
Corey <iam@coreylanier.com>
2019-08-24 21:23:23 -0400

    created initial state for tournament pages

--- src/components/Tournament.js
+++ src/components/Tournament.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
 import firebase from "../firebase";
 import Flag from "react-world-flags";
 
 class Tournament extends React.Component {
+  state = {
+    tournament: {}
+  };
+  async componentDidMount() {
+    const key = window.location.pathname.replace("/tournament/", "");
+    this.dbRefTournament = firebase.database().ref(`tournaments/${key}`);
+    await this.dbRefTournament.on("value", snapshot => {
+      const tournament = snapshot.val();
+      tournament.key = key;
+      const newDate = new Date(tournament.tournamentDate + "T00:00:00");
+      tournament.tournamentDate = new Intl.DateTimeFormat("en-us").format(
+        newDate
+      );
+      if (tournament.bracketApi === "challonge") {
+        tournament.bracketUrl = `https://challonge.com/${tournament.tournamentId}`;
+      } else if (tournament.bracketApi === "burningmeter") {
+        tournament.bracketUrl = `https://burningmeter.com/${tournament.tournamentId}`;
+      }
+      this.setState({ tournament });
+    });
+  }
   render() {
     return (
       <section className="tournament">
-        <h3>Tournament</h3>
+        <h3>{this.state.tournament.tournamentName}</h3>
       </section>
     );
   }
